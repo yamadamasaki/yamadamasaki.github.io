7525b8c53d15d693bf06adc467f851ff903e13bc
山田正樹 <masaki@metabolics.co.jp>
2016-01-25 19:12:07 +0900

    Updated site

--- config.rb
+++ config.rb
@@ -8,13 +8,13 @@ http_path = "/"
 http_images_path = "/images"
 http_generated_images_path = "/images"
 http_fonts_path = "/fonts"
-css_path = "/Users/yamadamasaki/Downloads/grain-theme-octopress-0.4.5/.cache/compass/stylesheets"
+css_path = "/Users/yamadamasaki/Dropbox/yamadamasaki.github.io/grain-theme-octopress-0.4.5/.cache/compass/stylesheets"
 
 # Local development paths
-sass_path = "/Users/yamadamasaki/Downloads/grain-theme-octopress-0.4.5/theme/sass"
-generated_images_path = "/Users/yamadamasaki/Downloads/grain-theme-octopress-0.4.5/.cache/compass/images"
-images_path = "/Users/yamadamasaki/Downloads/grain-theme-octopress-0.4.5/theme/images"
-fonts_path = "/Users/yamadamasaki/Downloads/grain-theme-octopress-0.4.5/theme/fonts"
+sass_path = "/Users/yamadamasaki/Dropbox/yamadamasaki.github.io/grain-theme-octopress-0.4.5/theme/sass"
+generated_images_path = "/Users/yamadamasaki/Dropbox/yamadamasaki.github.io/grain-theme-octopress-0.4.5/.cache/compass/images"
+images_path = "/Users/yamadamasaki/Dropbox/yamadamasaki.github.io/grain-theme-octopress-0.4.5/theme/images"
+fonts_path = "/Users/yamadamasaki/Dropbox/yamadamasaki.github.io/grain-theme-octopress-0.4.5/theme/fonts"
 
 line_comments = false
 output_style = :compressed
