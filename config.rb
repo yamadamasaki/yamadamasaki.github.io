edcf4283c3fa7ca893145589268b29429320f5d2
山田正樹 <masaki@metabolics.co.jp>
2016-01-25 18:31:01 +0900

    Updated site

---
+++ config.rb
@@ -0,0 +1,20 @@
+
+
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/"
+http_images_path = "/images"
+http_generated_images_path = "/images"
+http_fonts_path = "/fonts"
+css_path = "/Users/yamadamasaki/Downloads/grain-theme-octopress-0.4.5/.cache/compass/stylesheets"
+
+# Local development paths
+sass_path = "/Users/yamadamasaki/Downloads/grain-theme-octopress-0.4.5/theme/sass"
+generated_images_path = "/Users/yamadamasaki/Downloads/grain-theme-octopress-0.4.5/.cache/compass/images"
+images_path = "/Users/yamadamasaki/Downloads/grain-theme-octopress-0.4.5/theme/images"
+fonts_path = "/Users/yamadamasaki/Downloads/grain-theme-octopress-0.4.5/theme/fonts"
+
+line_comments = false
+output_style = :compressed
